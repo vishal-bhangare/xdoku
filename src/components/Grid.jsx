82ef2f0fb5cb16f23d75e57e5298778047523504
vishalBhangare <vishalbhangare2@gmail.com>
2023-11-25 18:12:19 +0530

    first commit

---
+++ src/components/Grid.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import { listToMatrix, solveSudoku } from "../functions";
+
+const Grid = ({ grid }) => {
+  const [solvedGrid, setSolvedGrid] = useState([[]]);
+  const [gridArr, setGridArr] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setGridArr([]);
+    const elements = e.target.elements;
+    for (let i = 0; i < 81; i++) {
+      const value = elements[i].value;
+      setGridArr((row) => [...row, value == "" ? 0 : parseInt(value)]);
+    }
+  };
+
+  useEffect(() => {
+    if (gridArr.length > 0) alert(solveSudoku(listToMatrix(gridArr, 9)));
+  }, [gridArr]);
+  return (
+    <>
+      {grid && (
+        <form onSubmit={handleSubmit}>
+          <table className="w-96 h-96 border-collapse border-2 border-slate-400 m-auto">
+            <tbody className="grid grid-cols-1 grid-rows-9 w-full h-full">
+              {grid?.map((row, idx) => (
+                <tr
+                  className={["grid grid-cols-9 grid-rows-1"].join(" ")}
+                  key={idx}
+                >
+                  {row.map((item, i) => (
+                    <td
+                      className={[
+                        "border-2 border-slate-400",
+                        (i + 1) % 3 == 0 && i < 8
+                          ? "border-r-2 border-r-slate-500"
+                          : "border-slate-400",
+                        ,
+                        (idx + 1) % 3 == 0 && idx < 8
+                          ? "border-b-2 border-b-slate-500"
+                          : "border-slate-400",
+                      ].join(" ")}
+                      key={i}
+                    >
+                      <input
+                        type="text"
+                        name={"element" + (idx + 1) + "/" + (i + 1)}
+                        defaultValue={item > 0 ? item : ""}
+                        pattern="[1-9]{1}"
+                        maxLength={1}
+                        className="text-center border-none outline-none w-full h-full"
+                      />
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <input
+            type="submit"
+            value={"submit"}
+            className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-2.5 py-1 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          />
+        </form>
+      )}
+    </>
+  );
+};
+
+export default Grid;
---
+++ src/functions.js
@@ -0,0 +1,164 @@
+export function listToMatrix(list, elementsPerSubArray) {
+  var matrix = [], i, k;
+
+  for (i = 0, k = -1; i < list.length; i++) {
+    if (i % elementsPerSubArray === 0) {
+      k++;
+      matrix[k] = [];
+    }
+
+    matrix[k].push(list[i]);
+  }
+
+  return matrix;
+}
+export function generateSudoku(blankCount) {
+  // @ts-ignore
+  const grid = Array.from({ length: 9 }, () => Array(9).fill(0));
+
+  // Fill the main diagonal of the grid
+  fillDiagonal(grid);
+
+  // Try to fill the entire grid
+  if (solveSudoku(grid)) {
+    if (blankCount)
+      createBlankSpaces(grid, blankCount)
+    return grid;
+  } else {
+    // If a solution is not possible, regenerate the grid
+    return generateSudoku();
+  }
+}
+
+function fillDiagonal(grid) {
+  for (let i = 0; i < 9; i += 3) {
+    fillBox(grid, i, i);
+  }
+}
+
+function fillBox(grid, row, col) {
+  const values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  shuffleArray(values);
+
+  let valIndex = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      grid[row + i][col + j] = values[valIndex];
+      valIndex++;
+    }
+  }
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function isSafe(grid, row, col, num) {
+  // Check if 'num' is not present in the current row, column, and the 3x3 subgrid
+  return (
+    !usedInRow(grid, row, num) &&
+    !usedInCol(grid, col, num) &&
+    !usedInBox(grid, row - (row % 3), col - (col % 3), num)
+  );
+}
+
+function usedInRow(grid, row, num) {
+  return grid[row].includes(num);
+}
+
+function usedInCol(grid, col, num) {
+  for (let i = 0; i < 9; i++) {
+    if (grid[i][col] === num) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function usedInBox(grid, startRow, startCol, num) {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (grid[startRow + i][startCol + j] === num) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function findUnassignedLocation(grid) {
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      if (grid[row][col] === 0) {
+        return { row, col };
+      }
+    }
+  }
+  return null;
+}
+
+export function solveSudoku(grid) {
+  if (grid.length < 9)
+    return false
+  const emptyLocation = findUnassignedLocation(grid);
+
+  if (!emptyLocation) {
+    // No unassigned location, the grid is solved
+    return true;
+  }
+
+  const { row, col } = emptyLocation;
+
+  for (let num = 1; num <= 9; num++) {
+    if (isSafe(grid, row, col, num)) {
+      // Try assigning the current number
+      grid[row][col] = num;
+
+      // Recursively try to solve the rest of the grid
+      if (solveSudoku(grid)) {
+        return true;
+      }
+
+      // If the current assignment does not lead to a solution, backtrack
+      grid[row][col] = 0;
+    }
+  }
+
+  // No number can be assigned, backtrack
+  return false;
+}
+export function createBlankSpaces(grid, blankCount) {
+  // Randomly remove numbers to create blank spaces
+  // const blankCount = Math.floor(Math.random() * 20) + 15;
+
+  for (let i = 0; i <= blankCount; i++) {
+    const row = Math.floor(Math.random() * 9);
+    const col = Math.floor(Math.random() * 9);
+
+    // Set the cell value to 0 to represent a blank space
+    grid[row][col] = 0;
+  }
+}
+// Example usage
+// const sudokuGrid = generateSudoku();
+// console.log(sudokuGrid);
+// createBlankSpaces(sudokuGrid, 40)
+// console.log(sudokuGrid);
+
+// @ts-ignore
+const gridElement = document.getElementById("grid");
+
+// for (let i = 0; i < 9; i++) {
+//   for (let j = 0; j < 9; j++) {
+//     let inputElement = document.createElement("input");
+//     inputElement.type = "text";
+//     inputElement.classList.add("element")
+//     if (sudokuGrid[i][j] != 0)
+//       inputElement.value = sudokuGrid[i][j];
+//     gridElement.appendChild(inputElement);
+//   }
+// }
+
